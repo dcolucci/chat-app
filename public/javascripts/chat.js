5faa4e69104b887ba8fcb47166ad4df8b0ab5f7f
David Colucci <davidanthonycolucci@gmail.com>
2014-10-27 18:06:07 -0400

    continued work on usernames

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,3 +7,14 @@ var Chat = function (socket) {
 Chat.prototype.sendMessage = function (messageText) {
   this.socket.emit('message', messageText);
 };
+
+Chat.prototype.processCommand = function (command) {
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  switch (command) {
+    case "sn":
+      this.socket.emit('screennameChangeRequest', args[0]);
+    default:
+      this.socket.emit('message', 'invalid command');
+  }
+}
